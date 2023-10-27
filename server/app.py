ae38c936b8e6d6e3718aac95073cd20e4a67efc8
Kimutai <jankimsingoei@gmail.com>
2023-10-27 13:27:55 +0300

    Progress

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session,jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,14 +28,39 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.get(session['user_id'])
+            return jsonify(user.to_dict()), 200
+        else:
+            return '', 204
+        
 
+api.add_resource(CheckSession,'/check_session')
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json().get('username')
+        password = request.get_json().get('password')
+        user = User.query.filter(User.username == username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return jsonify(user.to_dict()), 200
+        else:
+            return jsonify({}), 201
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {},204
+        # if session.get('user_id'):
+        #     session['user_id'] = None
+        #     #session.pop('user_id', None)
+        #     return {},204
+        # else:
+        #     return {"error":"Unauthorized access"},401
 
+api.add_resource(Logout,'/logout')
+api.add_resource(Login,'/login')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
